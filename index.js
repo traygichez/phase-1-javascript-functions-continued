c85a95317bbcd8f191497a88e1e204440df30902
tray <onesimus.githua@student.moringaschool.com>
2023-09-20 12:47:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(action =`roller-skate`){
+return(`This Saturday, I want to ${action}!`)
+}
+const mondayWork = function (activity="go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+function wrapAdjective(parameter1="*"){
+    return function( parameter="special"){
+        return (`You are ${parameter1}${parameter}${parameter1}!`)
+    }
+}
